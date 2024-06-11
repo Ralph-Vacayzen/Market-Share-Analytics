0628f63e0c4742154d178d2a17024d995102b5b2
Ralph McCracken <ralph@mccracken.work>
2024-06-11 14:47:34 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title='Market-share Analytics', page_icon='📊', layout="centered", initial_sidebar_state="auto", menu_items=None)
+
+
+st.caption('VACAYZEN')
+st.title('Market-share Analytics')
+st.info('Analysis of business areas to determine change in market share.')
+
